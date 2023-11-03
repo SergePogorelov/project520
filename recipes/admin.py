696712ffa186d3f8b663a6542c1bde5f70c37be5
Sergei Pogorelov <spogorelov@umass.edu>
2023-11-02 22:10:27 -0400

    Added recipe display classes for the admin panel and adjusted the application name for consistency.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Ingredient,
+    IngredientValue,
+    Recipe,
+    Subscription,
+    Favorite,
+)
+
+
+class IngredientValueInline(admin.StackedInline):
+    model = IngredientValue
+
+
+@admin.register(Recipe)
+class RecepieAdmin(admin.ModelAdmin):
+    inlines = [
+        IngredientValueInline,
+    ]
+    prepopulated_fields = {"slug": ("name",)}
+
+    list_display = ("name", "pub_date", "author", "display_favorites")
+
+    fieldsets = (
+        (None, {"fields": ("author", "name", "slug")}),
+        (
+            "Tags",
+            {
+                "fields": (
+                    (
+                        "breakfast",
+                        "lunch",
+                        "dinner",
+                    ),
+                )
+            },
+        ),
+        (
+            "Info and image",
+            {
+                "fields": (
+                    "cooking_time",
+                    "description",
+                    "image",
+                ),
+            },
+        ),
+    )
+
+    list_filter = ("author", "name", "breakfast", "lunch", "dinner")
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("title", "dimension")
+    search_fields = ("title",)
+
+
+admin.site.register(Subscription)
+admin.site.register(Favorite)
